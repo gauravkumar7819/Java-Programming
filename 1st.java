b6c61b14bd2bde1b23df738a9c3b82fa620f70b6
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2023-11-04 07:44:46 +0530

    java codes

---
+++ 1st.java
@@ -0,0 +1,19 @@
+public class st{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] s1=new int [4];
+String s="abcd";
+		for(int i=0;i<3;i++) {
+			s1[i]=s.charAt(i);
+			
+			
+		}
+		
+		for(int i:s1) {
+			System.out.println(s1[i]);
+		}
+	
+	}
+
+}
